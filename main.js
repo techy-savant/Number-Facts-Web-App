54bf8cac76df1b8c7d3dddc811387acb05b4a730
Chukwuemeka-OGB <ogbanufavour@gmail.com>
2023-06-12 07:56:21 +0100

    changed data fethcing method from ajax to fetch

--- main.js
+++ main.js
@@ -6,25 +6,42 @@ let btn = document.querySelector(".btn");
 let output = document.createElement('p'); //create output element
 output.classList.add('box');
 
-btn.addEventListener('click', fetchFactAjax) || form.addEventListener('submit', fetchFactAjax);
+btn.addEventListener('click', getFactFetch) || form.addEventListener('submit', getFactFetch);
 
-function fetchFactAjax(e){
+// function fetchFactAjax(e){
+//     e.preventDefault();
+//     let number = numberInput.value;
+//     if (isNaN(number) || number < 0) { //automatically make value 1 if it's a negative number
+//       number = 1;
+//     }
+   
+//     let xhr = new XMLHttpRequest(); //create xhr request
+//     xhr.open('GET', `http://numbersapi.com/${number}`); //specify fetch method and source site
+
+//     //most of the main stuff is done here
+//     xhr.onload = function(){
+//         if(this.status == 200 && number != ''){ //if server is running legitimately and entry is not blank, then perform action
+//             output.textContent = this.responseText;
+//             boxes.appendChild(output);
+//         }
+//     };
+//     xhr.send();
+// }
+
+function getFactFetch(e){
     e.preventDefault();
     let number = numberInput.value;
     if (isNaN(number) || number < 0) { //automatically make value 1 if it's a negative number
-      number = 1;
+        number = 1;
     }
-   
-    let xhr = new XMLHttpRequest(); //create xhr request
-    xhr.open('GET', `http://numbersapi.com/${number}`); //specify fetch method and source site
 
-    //most of the main stuff is done here
-    xhr.onload = function(){
-        if(this.status == 200 && number != ''){ //if server is running legitimately and entry is not blank, then perform action
-            output.textContent = this.responseText;
-            boxes.appendChild(output);
+    fetch(`http://numbersapi.com/${number}`)
+    .then(res => res.text())
+    .then(data => {
+        if(number != ''){ //if entry is not blank, then perform action
+         output.textContent = data;
+        boxes.appendChild(output);
         }
-    };
-    xhr.send();
+    })
+    .catch(err => console.log(err));
 }
-
